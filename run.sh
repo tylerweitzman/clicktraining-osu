2c504aab95907a16a0a107aa05aa25a0d31696c7
tyler <tylerweitzman@gmail.com>
2024-08-19 09:46:15 -0400

    Refactoring

---
+++ run.sh
@@ -0,0 +1,8 @@
+# There are no dependencies to install for this codebase as it is a static HTML/JS application.
+
+# Serve the current directory using a simple static server like python's built-in HTTP server.
+# Python 2.x
+python -m SimpleHTTPServer 8000 &
+
+# Python 3.x
+python3 -m http.server 8000 &
